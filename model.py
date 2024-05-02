03231be0bbda1fef22a5dfae181da78cbd26e23d
dina-321 <162456097+dina-321@users.noreply.github.com>
2024-05-02 15:00:19 +0300

    Update model.py

--- model.py
+++ model.py
@@ -1,18 +1,16 @@
-from flask import Flask, render_template
+
+
+from flask import Flask
 from flask_socketio import SocketIO
-from function import detect_cheating
+from function import detect_cheating_realtime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
-
 @socketio.on('get_detection_results')
 def get_detection_results():
-    detect_cheating(socketio)  # Pass socketio to detect_cheating function
+    detect_cheating_realtime(socketio)  # Pass socketio to detect_cheating function
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
