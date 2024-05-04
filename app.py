dd20e9715093bc6af2fde0d5c799983f7e25d325
Abdelrahman435 <ah0383723@gmail.com>
2024-05-05 02:50:03 +0300

    html

--- app.py
+++ app.py
@@ -1,21 +1,29 @@
-
-from flask import Flask, request, jsonify
-from flask_socketio import SocketIO
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 from function import detect_cheating
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
-@app.route('/detect_cheating', methods=['POST'])
-def detect_cheating_route():
-    detection_results_df = detect_cheating(socketio)  # Trigger detection process
-    detection_results_dict = detection_results_df.to_dict(orient='records')  # Convert DataFrame to dictionary
-    return jsonify(detection_results_dict)  # Convert dictionary to JSON and return as response
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @socketio.on('connect')
 def handle_connect():
     print('Client connected')
+
+@socketio.on('get_detection_results')
+def get_detection_results():
+    detection_results = detect_cheating()
+    
+    # Convert Timestamp objects to strings
+    detection_results['Time'] = detection_results['Time'].astype(str)
+    
+    # Emit the modified DataFrame as a list of dictionaries
+    emit('update_detection_results', detection_results.to_dict(orient='records'))
+
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
