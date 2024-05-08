05fc8ac7d91a7a95cc497e2c61322b9e8f0ac5df
dina-321 <162456097+dina-321@users.noreply.github.com>
2024-05-08 18:21:21 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
-
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from function import detect_cheating
 import tempfile
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/detect', methods=['POST'])
 def detect():
     if request.method == 'POST':
-        if 'image_files' not in request.files:
+        if 'imagefiles' not in request.files:
             return jsonify({'error': 'No files uploaded'})
-        
-        image_files = request.files.getlist('image_files')
+
+        image_files = request.files.getlist('imagefiles')
         if len(image_files) == 0:
             return jsonify({'error': 'No files uploaded'})
-        
+
         # Save uploaded files temporarily
         temp_file_paths = []
         for image_file in image_files:
@@ -23,14 +24,14 @@ def detect():
             temp_file_paths.append(temp_file.name)
             image_file.save(temp_file)
             temp_file.close()
-        
+
         # Perform cheating detection
         results = detect_cheating(temp_file_paths)
-        
+
         # Delete temporary files
         for temp_file_path in temp_file_paths:
             os.unlink(temp_file_path)
-        
+
         return jsonify(results)
 
 if __name__ == '__main__':
